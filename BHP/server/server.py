6c583c0236c7fea6557495e870bb690e8569a4ba
Diya Burman <diyaburman60@gmail.com>
2024-12-29 23:11:39 +0530

    server updated

--- BHP/server/server.py
+++ BHP/server/server.py
@@ -1,8 +1,16 @@
 from flask import Flask, request, jsonify
+import util
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
+@app.route('/get_location_names')
+def get_location_names():
+    response = jsonify({
+        'location': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
     return "Hi"
 
 
