d5bcf7f181b58905aec7c6823ae19afc2c06a144
Diya Burman <diyaburman60@gmail.com>
2024-12-29 23:32:22 +0530

    server updated

--- BHP/server/util.py
+++ BHP/server/util.py
@@ -1,8 +1,30 @@
 import json
 import pickle
+import numpy as np
+
 __loaction = None
 __data_columns = None
 __model = None
+
+def get_estimated_price(location,sqft,bhk,bath):
+
+    # Find the index of the location column
+    try:
+        loc_index = __data_columns.index(location.lower())  # Get the index of the location
+    except ValueError:
+        loc_index = -1  # If location is not found
+
+    # Create a NumPy array of zeros with the same length as column_names
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+
+    # Set the location index to 1 if found
+    if loc_index >= 0:
+        x[loc_index] = 1
+
+    return round(__model.predict([x])[0],2)
 
 def get_location_names():
     return __locations
@@ -15,6 +37,7 @@ def load_saved_artifacts():
     with open("./artifacts/columns.json", 'r') as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
+    global __model
 
     with open("./artifacts/banglore_home_prices_model.pickle", 'rb') as f:
         __model = pickle.load(f)
@@ -23,3 +46,7 @@ def load_saved_artifacts():
 if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
+    print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
